50ddb3faeda0104a7c959bd75b608c6da85b7b02
zhengc <zhengc@peoplecommnue.com>
2021-05-24 14:27:59 +0800

    emacs配置文件拆分模块化

--- init.el
+++ init.el
@@ -1,12 +1,12 @@
-(setq inhibit-startup-message t)
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+(require 'init-ui)
 
+(setq inhibit-startup-message t)
 (require 'package)
 (setq package-enable-at-startup nil)
 (setq use-package-always-ensure t)
+
 ;;(add-to-list 'package-archives
-
-
-
 ;;	     '("melpa" . "http://elpa.emacs-china.org/melpa/"))
 ;; (setq package-archives '(
 ;; 			             ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
@@ -23,13 +23,6 @@
 ;;           ("https" . "127.0.0.1:52067")))
 (package-initialize)
 
-;;隐藏菜单栏
-(menu-bar-mode nil)
-;;隐藏工具栏
-;;(tool-bar-mode nil)
-(tool-bar-mode -1)
-;;隐藏滚动条
-(scroll-bar-mode nil)
 
 
 ;;设置默认目录
@@ -94,34 +87,6 @@
 ;;     (setq use-package-always-defer t) ;默认都是延迟加载，不用每个包都手动添加:defer t
 ;;     )
 
-;;==========mode line 美化显示==========
-(use-package nyan-mode
-  :ensure t
-  :init
-  (setq nyan-animate-nyancat t)
-  (setq nyan-wavy-trail t)
-  (setq nyan-minimum-window-width 80)
-  (setq nyan-bar-length 20)
-  (nyan-mode))
-
-
-(use-package doom-modeline
-  :ensure t
-  :config
-  (doom-modeline-def-modeline 'ownml
-    '(bar matches buffer-info remote-host buffer-position misc-info major-mode) 
-    '(input-method buffer-encoding process vcs checker))
-
-  ;; Add to `doom-modeline-mode-hook` or other hooks
-  (defun setup-custom-doom-modeline ()
-    (doom-modeline-set-modeline 'ownml 'default))
-  (add-hook 'doom-modeline-mode-hook 'setup-custom-doom-modeline)
-  )
-
-
-(doom-modeline-mode t)
-;;==========mode line 美化显示 end==========
-
 
 (use-package leetcode
   :config
@@ -148,23 +113,7 @@
 
 
 ;;==========skybert==========
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Show paren mode, built-in from Emacs 24.x   光标悬停代码块加底色
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(show-paren-mode t)
-(setq show-paren-style 'expression)
 
-(use-package paren)
-(set-face-background 'show-paren-match (face-background 'default))
-(set-face-attribute 'show-paren-match nil :weight 'extra-bold)
-
-;; Sub word support
-(add-hook 'minibuffer-setup-hook 'subword-mode)
-;;;;;;;;;;;;;;;;;;;;;;;;;skybert;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; ws-butler cleans up whitespace only on the lines you've edited,
-;; keeping messy colleagues happy ;-) Important that it doesn't clean
-;; the whitespace on currrent line, otherwise, eclim leaves messy
-;; code behind.
 (use-package ws-butler
   :init
   (setq ws-butler-keep-whitespace-before-point nil)
@@ -784,11 +733,6 @@
 ;; 	 )
 ;;   )
 
-(use-package dracula-theme
-  :ensure t
-  )
-
-(load-theme 'dracula t)
 
 
 (use-package format-all
@@ -988,7 +932,8 @@
 
 ;;字体设置
 ;;(set-face-attribute 'default nil :font "Menlo-16")
-(add-to-list 'default-frame-alist '(font . "DejaVu Sans Mono-16"))
+
+
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (when (file-exists-p custom-file)
   (load custom-file))
---
+++ lisp/init-ui.el
@@ -0,0 +1,66 @@
+;;隐藏菜单栏
+(menu-bar-mode nil)
+;;隐藏工具栏
+;;(tool-bar-mode nil)
+(tool-bar-mode -1)
+;;隐藏滚动条
+(scroll-bar-mode nil)
+
+;;==========theme==========
+(use-package dracula-theme
+  :ensure t
+  )
+
+(load-theme 'dracula t)
+;;==========theme end==========
+
+(add-to-list 'default-frame-alist '(font . "DejaVu Sans Mono-16"))
+
+;;==========mode line 美化显示==========
+(use-package nyan-mode
+  :ensure t
+  :init
+  (setq nyan-animate-nyancat t)
+  (setq nyan-wavy-trail t)
+  (setq nyan-minimum-window-width 80)
+  (setq nyan-bar-length 20)
+  (nyan-mode))
+
+
+(use-package doom-modeline
+  :ensure t
+  :config
+  (doom-modeline-def-modeline 'ownml
+    '(bar matches buffer-info remote-host buffer-position misc-info major-mode) 
+    '(input-method buffer-encoding process vcs checker))
+
+  ;; Add to `doom-modeline-mode-hook` or other hooks
+  (defun setup-custom-doom-modeline ()
+    (doom-modeline-set-modeline 'ownml 'default))
+  (add-hook 'doom-modeline-mode-hook 'setup-custom-doom-modeline)
+  )
+
+
+(doom-modeline-mode t)
+;;==========mode line 美化显示 end==========
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Show paren mode, built-in from Emacs 24.x   光标悬停代码块加底色
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(show-paren-mode t)
+(setq show-paren-style 'expression)
+
+(use-package paren)
+(set-face-background 'show-paren-match (face-background 'default))
+(set-face-attribute 'show-paren-match nil :weight 'extra-bold)
+
+;; Sub word support
+(add-hook 'minibuffer-setup-hook 'subword-mode)
+;;;;;;;;;;;;;;;;;;;;;;;;;skybert;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ws-butler cleans up whitespace only on the lines you've edited,
+;; keeping messy colleagues happy ;-) Important that it doesn't clean
+;; the whitespace on currrent line, otherwise, eclim leaves messy
+;; code behind.
+
+(provide 'init-ui)
