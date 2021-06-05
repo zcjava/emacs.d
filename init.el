b38bdff175867547be68e6ec2906050ff21037eb
zhengc <zhengc@peoplecommnue.com>
2021-06-05 15:21:27 +0800

    update

--- init.el
+++ init.el
@@ -14,9 +14,10 @@
 (require 'init-lsp)
 (require 'init-lsp-java)
 (require 'init-lsp-flutter)
+
 (require 'init-python)
 (require 'init-leetcode)
-
+(require 'init-nginx)
 (require 'init-my)
 
 
---
+++ lisp/init-nginx.el
@@ -0,0 +1,4 @@
+(use-package nginx-mode)
+(use-package company-nginx)
+
+(provide 'init-nginx)
