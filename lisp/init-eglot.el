8e4b4d556412f93517a34963f4a0a9fbe46855fa
zcjava <zcjava@qq.com>
2022-07-18 12:12:26 +0800

    update

--- lisp/init-eglot.el
+++ lisp/init-eglot.el
@@ -2,17 +2,22 @@
   "A major mode derived from web-mode, for editing .vue files with LSP support.")
 
 
-(defun my-eglot-keybindgs ()
-  (define-key evil-motion-state-map "gR" #'eglot-rename)
-  (define-key evil-motion-state-map "gr" #'xref-find-references)
-  (define-key evil-normal-state-map "gi" #'eglot-find-implementation)
-  (define-key evil-motion-state-map "gh" #'eldoc)
-  (define-key evil-normal-state-map "ga" #'eglot-code-actions))
+;; (defun my-eglot-keybindgs ()
+;;   (define-key evil-motion-state-map "gR" #'eglot-rename)
+;;   (define-key evil-motion-state-map "gr" #'xref-find-references)
+;;   (define-key evil-normal-state-map "gi" #'eglot-find-implementation)
+;;   (define-key evil-motion-state-map "gh" #'eldoc)
+;;   (define-key evil-normal-state-map "ga" #'eglot-code-actions))
 
-(use-package prettier-js
-  :ensure t
-  )
-
+;; (use-package prettier-js
+;;   :ensure t
+;;   :config
+;;   (setq prettier-js-args '(
+;;                            "--print-width" "100"  ;;一行代码的最大字符数,默认是80
+;;                            "--trailing-comma" "all"  ;; 尾部逗号处理
+;;                            "--bracket-spacing" "false" ;; > 是否另起一行
+;;                            ))
+;;   )
 
 (use-package web-mode
   :ensure t
@@ -26,7 +31,7 @@
   :init
   (add-to-list 'auto-mode-alist '("\\.vue\\'" . genehack-vue-mode))
   (add-to-list 'auto-mode-alist '("\\.\\(html\\|css\\)\\'" . web-mode))
-  (advice-add 'eglot-ensure :after 'my-eglot-keybindgs)
+  ;;  (advice-add 'eglot-ensure :after 'my-eglot-keybindgs)
   :bind (:map eglot-mode-map
               ("C-c l a" . eglot-code-actions)
               ("C-c l r" . eglot-rename)
